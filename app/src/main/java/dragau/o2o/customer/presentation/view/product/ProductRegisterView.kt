77240faea3e0ac6f59f50abb34d72bf5f588e025
Pavel <pavelmaneyev@gmail.com>
2019-10-24 12:43:23 +0600

    state

--- app/src/main/java/dragau/o2o/customer/presentation/view/product/ProductRegisterView.kt
+++ app/src/main/java/dragau/o2o/customer/presentation/view/product/ProductRegisterView.kt
@@ -1,8 +1,11 @@
 package dragau.o2o.customer.presentation.view.product
 
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import dragau.o2o.customer.presentation.BaseView
 
 
 interface ProductRegisterView : BaseView {
+    @StateStrategyType(SkipStrategy::class)
     fun showPictureDialog()
 }
