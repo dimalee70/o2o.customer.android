ab1cb3f12f3745ec242f37e67756256b1f8a499f
dmitriy <150107016@stu.sdu.edu.kz>
2019-10-24 15:20:41 +0600

    Added product show page

--- app/src/main/java/dragau/o2o/customer/presentation/view/product/ProductRegisterView.kt
+++ app/src/main/java/dragau/o2o/customer/presentation/view/product/ProductRegisterView.kt
@@ -1,8 +1,12 @@
 package dragau.o2o.customer.presentation.view.product
 
+import com.arellomobile.mvp.viewstate.strategy.SkipStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 import dragau.o2o.customer.presentation.BaseView
 
 
 interface ProductRegisterView : BaseView {
+
+    @StateStrategyType(SkipStrategy::class)
     fun showPictureDialog()
 }
