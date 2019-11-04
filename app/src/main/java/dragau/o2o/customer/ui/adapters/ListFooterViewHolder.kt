09d648521d5b7a92ebc6d1d5c761340a4696c2b5
dmitriy <150107016@stu.sdu.edu.kz>
2019-11-04 11:04:52 +0600

    Changed item view

--- app/src/main/java/dragau/o2o/customer/ui/adapters/ListFooterViewHolder.kt
+++ app/src/main/java/dragau/o2o/customer/ui/adapters/ListFooterViewHolder.kt
@@ -7,13 +7,17 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import dragau.o2o.customer.R
 import dragau.o2o.customer.models.enums.PagingState
+import dragau.o2o.customer.models.objects.Product
 import kotlinx.android.synthetic.main.item_list_footer.view.*
 
 class ListFooterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun bind(status: PagingState?) {
+    fun bind(status: PagingState?, onItemClickListener: ProductsListAdapter.OnItemClickListener<Product>) {
         itemView.progress_bar.visibility = if (status == PagingState.LOADING) VISIBLE else View.INVISIBLE
         itemView.txt_error.visibility = if (status == PagingState.ERROR) VISIBLE else View.INVISIBLE
+        itemView.setOnClickListener{
+            onItemClickListener
+        }
     }
 
     companion object {
